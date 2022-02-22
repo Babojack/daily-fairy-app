1784099f41fde4c7aa8643592ebc338b28782607
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-02-22 12:21:00 +0100

    booking model created and migrated

---
+++ db/migrate/20220222112029_create_bookings.rb
@@ -0,0 +1,12 @@
+class CreateBookings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :bookings do |t|
+      t.date :start_date
+      t.date :end_date
+      t.references :user, null: false, foreign_key: true
+      t.references :fairy, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_111629) do
+ActiveRecord::Schema.define(version: 2022_02_22_112029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id", null: false
+    t.bigint "fairy_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["fairy_id"], name: "index_bookings_on_fairy_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "fairies", force: :cascade do |t|
     t.string "name"
@@ -38,5 +49,7 @@ ActiveRecord::Schema.define(version: 2022_02_22_111629) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "fairies"
+  add_foreign_key "bookings", "users"
   add_foreign_key "fairies", "users"
 end
