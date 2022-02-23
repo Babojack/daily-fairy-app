9dc82713dd121ea02f7595efcf2ec90c28f3066b
murjana younis <murjana.younis@gmail.com>
2022-02-23 11:39:20 +0100

    routes set up for booking

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :fairies, only: [:index, :show]
+  resources :fairies, only: [:index, :show] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
