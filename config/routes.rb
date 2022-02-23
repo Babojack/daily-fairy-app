e57adef07169404ef31d6f850829faf9a041683e
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-02-23 14:54:42 +0100

    new update

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :fairies, only: [:index, :show] do
     resources :bookings, only: [:new, :create]
   end
+
+  resources :fairies, only: [:new, :create]
+  
   resources :bookings, only: [:show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
