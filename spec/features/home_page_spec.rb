a21551dde7bc92d634ead21ad3969f8f4f3f8cff
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-11-26 00:40:14 +0100

    user can see some unclickable texts on the homepage

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,7 +1,11 @@
 require "rails_helper"
 
 RSpec.feature "home page" do
-  scenario "user can see the link that leads to all fairies" do
+  scenario "user can see some unclickable texts" do
     visit "/"
+    expect(page).to have_content("Find your muse")
+    expect(page).to have_content("DAILY FAIRIES")
+    expect(page).to have_content("Magical creatures for cheap people!")
+    expect(page).to have_content("Change your life with magic by renting one of our fairies from around the universe.")
   end
 end
