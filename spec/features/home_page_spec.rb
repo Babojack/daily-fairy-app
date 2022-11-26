b8c1155c86c17309959ba01d2e2f4e86217948e0
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-11-26 22:20:42 +0100

    user can click on view fairies to display information of all fairies

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,11 +1,18 @@
 require "rails_helper"
 
-RSpec.feature "home page" do
-  scenario "user can see some unclickable texts" do
+RSpec.describe "home page" do
+  before(:each) do
     visit "/"
+  end
+  it "user can see some unclickable texts" do
     expect(page).to have_content("Find your muse")
     expect(page).to have_content("DAILY FAIRIES")
     expect(page).to have_content("Magical creatures for cheap people!")
     expect(page).to have_content("Change your life with magic by renting one of our fairies from around the universe.")
+  end
+
+  it "displays information for all fairies" do
+    click_on "VIEW FAIRIES"
+    expect(current_path).to eq(fairies_path)
   end
 end
