b43bef25f43a8e4d075e2cbfa87429cd16330d9f
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-02-15 15:40:37 +0100

    Modified the production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.assets.compile = true
+  config.public_file_server.enabled = true
+
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'https://daily-fairy.herokuapp.com/'
